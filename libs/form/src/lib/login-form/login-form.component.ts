b4dff77e6657af42b767858585472f8a8bb2f9cf
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-21 20:12:16 +0100

    feat: Create an example of form.

--- libs/form/src/lib/login-form/login-form.component.ts
+++ libs/form/src/lib/login-form/login-form.component.ts
@@ -1,7 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lib-login-form',
   templateUrl: './login-form.component.html',
+  imports: [
+    ReactiveFormsModule
+  ]
 })
-export class LoginFormComponent {}
+export class LoginFormComponent {
+  loginForm: FormGroup;
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      login: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+}
