91088e8d48edc24537a57a77bb920ecb36174407
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-21 19:52:42 +0100

    feat: Create the login form component.

---
+++ libs/form/src/lib/login-form/login-form.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-login-form',
+  templateUrl: './login-form.component.html',
+})
+export class LoginFormComponent {}
--- libs/view/src/lib/login-view/login-view.component.ts
+++ libs/view/src/lib/login-view/login-view.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
+import { LoginFormComponent } from '@vet-client/lib-form';
+
 @Component({
   selector: 'lib-login-view',
+  imports: [LoginFormComponent],
   templateUrl: './login-view.component.html',
 })
 export class LoginViewComponent {}
