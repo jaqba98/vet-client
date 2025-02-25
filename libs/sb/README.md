1e29db0d3e49ea6dfb5cbfcf07bd9ba750752a64
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-25 23:11:51 +0100

    refactor: rename the sb to lib-sb

--- libs/sb/README.md
+++ libs/sb/README.md
@@ -1,7 +1,7 @@
-# sb
+# lib-sb
 
 This library was generated with [Nx](https://nx.dev).
 
 ## Running unit tests
 
-Run `nx test sb` to execute the unit tests.
+Run `nx test lib-sb` to execute the unit tests.
--- libs/sb/jest.config.ts
+++ libs/sb/jest.config.ts
@@ -1,5 +1,5 @@
 export default {
-  displayName: 'sb',
+  displayName: 'lib-sb',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../coverage/libs/sb',
