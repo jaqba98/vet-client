6a99f47c0712643eca1007b405b1e3c3ebe60787
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-25 23:33:31 +0100

    fix: Fix the lib-sb configuration.

--- libs/view/src/index.ts
+++ libs/view/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/login-view/login-view.component';
+export * from './lib/top-nav-view/top-nav-view.component';
---
+++ libs/view/src/lib/top-nav-view/top-nav-view.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-top-nav-view',
+  templateUrl: './top-nav-view.component.html',
+})
+export class TopNavViewComponent {}
