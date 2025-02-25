784f89d8012ec35c1f7289ae304406cbc90f74c9
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-25 13:55:35 +0100

    feat: Create sb project with storybook configuration.

---
+++ libs/sb/README.md
@@ -0,0 +1,7 @@
+# sb
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test sb` to execute the unit tests.
---
+++ libs/sb/src/lib/page/root.component.stories.ts
@@ -0,0 +1,11 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { RootPageComponent } from '@vet-client/lib-page';
+
+const meta: Meta<RootPageComponent> = {
+  component: RootPageComponent,
+  title: 'Page/Root',
+};
+export default meta;
+type Story = StoryObj<RootPageComponent>;
+
+export const Default: Story = {};
