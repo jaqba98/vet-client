ae4ac19f897d6aefa788e165848f73732a203ec3
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-27 23:45:08 +0100

    refactor: Section View Component.

--- libs/control/src/lib/section-control/section-control.component.ts
+++ libs/control/src/lib/section-control/section-control.component.ts
@@ -1,3 +1,4 @@
+// done
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
@@ -10,56 +11,30 @@ import { BaseComponentDirective } from '@vet-client/lib-utils';
   styleUrl: './section-control.component.scss',
   hostDirectives: [BaseComponentDirective]
 })
-/** Section Control */
 export class SectionControlComponent {
   @Input({ required: true }) sectionId!: string;
 
-  @Input() marginAutoMax960px = false;
+  @Input() sectionContentMarginAuto = false;
 
-  @Input() marginAutoMax480px = false;
-
-  @Input() backgroundPrimary = false;
-
-  @Input() background = false;
-
-  @Input() backgroundSurface = false;
-
-  @Input() backgroundPrimary80 = false;
-
-  @Input() backgroundImageGuineaPig = false;
-
-  @Input() minHeight640 = false;
-
-  @Input() flexCenter = false;
-
-  @Input() fullHeightWithNav = false;
+  @Input() sectionContentMaxWidth480px = false;
 
   getSectionClassList() {
     return {
-      'section--background-image-guinea-pig': this.backgroundImageGuineaPig,
-      'section--min-height640': this.minHeight640,
-      'section--full-height-with-nav': this.fullHeightWithNav
+      section: true
     };
   }
 
   getSectionBackgroundClassList() {
     return {
-      'section__background--primary': this.backgroundPrimary,
-      'section__background--background': this.background,
-      'section__background--surface': this.backgroundSurface,
-      'section__background--primary80': this.backgroundPrimary80,
-      'section__background--min-height640': this.minHeight640,
-      'section__background--full-height-with-nav': this.fullHeightWithNav
+      section__background: true
     };
   }
 
   getSectionContentClassList() {
     return {
-      'section__content--margin-auto-max-960px': this.marginAutoMax960px,
-      'section__content--margin-auto-max-480px': this.marginAutoMax480px,
-      'section__content--min-height640': this.minHeight640,
-      'section__content--flex-center': this.flexCenter,
-      'section__content--full-height-with-nav': this.fullHeightWithNav
+      section__content: true,
+      'section__content--margin-auto': this.sectionContentMarginAuto,
+      'section__content--max-width-480px': this.sectionContentMaxWidth480px,
     };
   }
 }
