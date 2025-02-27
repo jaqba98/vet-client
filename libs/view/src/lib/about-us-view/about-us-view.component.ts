385d78073891280106982d385256d9ef83199291
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-27 09:39:48 +0100

    refactor: About us view.

--- libs/view/src/lib/about-us-view/about-us-view.component.ts
+++ libs/view/src/lib/about-us-view/about-us-view.component.ts
@@ -1,11 +1,14 @@
 import { Component } from "@angular/core";
 
 import { SectionControlComponent } from '@vet-client/lib-control';
+import { BaseComponentDirective } from '@vet-client/lib-utils';
 
 @Component({
   selector: 'lib-about-us-view',
   imports: [SectionControlComponent],
   templateUrl: './about-us-view.component.html',
-  styleUrl: './about-us-view.component.scss'
+  styleUrl: './about-us-view.component.scss',
+  hostDirectives: [BaseComponentDirective]
 })
+/** About Us View */
 export class AboutUsViewComponent {}
