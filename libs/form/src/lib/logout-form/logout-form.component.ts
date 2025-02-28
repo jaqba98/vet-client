288f15f4edc1d2be98b408b012e37fcc22c3a568
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-28 20:54:16 +0100

    feat: Create logout request.

--- libs/form/src/lib/logout-form/logout-form.component.ts
+++ libs/form/src/lib/logout-form/logout-form.component.ts
@@ -1,19 +1,33 @@
 // done
 import { Component } from '@angular/core';
 
-import { BaseFormComponent, BaseFormService } from '@vet-client/lib-system';
-import { LogoutFormDataModel, LogoutFormModel } from './logout-form.model';
+import {
+  BaseFormComponent,
+  BaseFormService,
+  CookieService,
+  HttpEndpointEnum,
+  HttpMethodEnum,
+  HttpService,
+  LogoutPostHttpResponseModel,
+  RouterEnum,
+  RouterService,
+} from '@vet-client/lib-system';
 import { BaseComponentDirective } from '@vet-client/lib-utils';
+import { LogoutFormDataModel, LogoutFormModel } from './logout-form.model';
 
 @Component({
   selector: 'lib-logout-form',
   imports: [BaseFormComponent],
   templateUrl: './logout-form.component.html',
   styleUrl: './logout-form.component.scss',
-  hostDirectives: [BaseComponentDirective]
+  hostDirectives: [BaseComponentDirective],
 })
 export class LogoutFormComponent extends BaseFormService<LogoutFormModel, LogoutFormDataModel> {
-  constructor() {
+  constructor(
+    private readonly http: HttpService,
+    private readonly cookie: CookieService,
+    private readonly router: RouterService
+  ) {
     super({
       logout: {
         id: 'logout',
@@ -23,12 +37,25 @@ export class LogoutFormComponent extends BaseFormService<LogoutFormModel, Logout
           text: 'Logout',
         },
         defaultValue: false,
-        fullWidth: false
+        fullWidth: false,
       },
     });
   }
 
   override onSubmit(model: LogoutFormDataModel) {
-    console.log(model);
+    this.http
+      .execute<LogoutPostHttpResponseModel>({
+        method: HttpMethodEnum.post,
+        type: {
+          endpoint: HttpEndpointEnum.logout,
+          request: { logout: model.logout },
+        },
+      })
+      .subscribe((response) => {
+        if (response.logout) {
+          this.cookie.deleteCookie('token');
+          this.router.redirect(RouterEnum.home);
+        }
+      });
   }
 }
