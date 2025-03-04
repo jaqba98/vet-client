22e02eee399274de09c7514cb32989361f0956b7
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-03-04 13:53:40 +0100

    feat: Add styles to the  dashboard panel.

--- libs/view/src/lib/dashboard-view/dashboard-view.component.ts
+++ libs/view/src/lib/dashboard-view/dashboard-view.component.ts
@@ -2,16 +2,31 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BaseComponentDirective } from '@vet-client/lib-utils';
+import { ButtonControlComponent, ButtonControlModel } from '@vet-client/lib-control';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'lib-dashboard-view',
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonControlComponent],
   templateUrl: './dashboard-view.component.html',
   styleUrl: './dashboard-view.component.scss',
   hostDirectives: [BaseComponentDirective]
 })
 export class DashboardViewComponent {
   isOpen = false;
+
+  buttonModel: ButtonControlModel = {
+    id: 'hamburger',
+    value: {
+      type: 'icon',
+      icon: {
+        icon: faBars,
+        color: 'light-primary',
+        fontSize: '1rem'
+      },
+    },
+    fullWidth: false
+  };
 
   onClick() {
     this.isOpen = !this.isOpen;
