6ec5bbbc6245690072b2912e59dc24f1f57df0a6
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-03-07 23:14:53 +0100

    feat: Rename the vet services form to vet clinic form

--- libs/view/src/lib/vet-clinic-view/vet-clinic-view.component.ts
+++ libs/view/src/lib/vet-clinic-view/vet-clinic-view.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core'
 import { SectionControlComponent } from '@vet-client/lib-control'
 import { RouteSectionEnum } from '@vet-client/lib-store'
 import { BaseComponentDirective } from '@vet-client/lib-utils'
+import { VetClinicFormComponent } from '@vet-client/lib-form'
 
 @Component({
   selector: 'lib-vet-clinic-view',
-  imports: [SectionControlComponent],
+  imports: [SectionControlComponent, VetClinicFormComponent],
   templateUrl: './vet-clinic-view.component.html',
   hostDirectives: [BaseComponentDirective],
 })
