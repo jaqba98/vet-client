d6d23fa7605cce379fa68b4bc1be0c249f374942
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-03-21 10:24:35 +0100

    feat: Create env project with api url for dev and prod.

---
+++ libs/env/src/lib/environment.prod.ts
@@ -0,0 +1,5 @@
+import { EnvironmentModel } from './environment.model'
+
+export const environment: EnvironmentModel = {
+  apiUrl: 'http://localhost:8080/api/v1/',
+}
--- libs/http/src/lib/infrastructure/http-execute.service.ts
+++ libs/http/src/lib/infrastructure/http-execute.service.ts
@@ -2,27 +2,22 @@ import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
 
+import { environment } from '@vet-client/lib-env'
 import { HttpType } from '../model/http.model'
 import { MethodEnum } from '../enum/method.enum'
-import { EndpointEnum } from '../enum/endpoint.enum'
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class HttpExecuteService {
   constructor(private http: HttpClient) {
   }
 
   exec<TResponse>(type: HttpType): Observable<TResponse> {
-    // I am here
-    const apiUrl = this.buildApiUrl(type.type.endpoint)
+    const apiUrl = `${environment.apiUrl}${type.type.endpoint}`
     switch (type.method) {
       case MethodEnum.post:
         return this.http.post<TResponse>(apiUrl, type.type.request)
       default:
         throw new Error(`Unsupported method ${type.method}`)
     }
-  }
-
-  private buildApiUrl(endpoint: EndpointEnum) {
-    return `http://localhost:8080/api/v1/${endpoint}`
   }
 }
