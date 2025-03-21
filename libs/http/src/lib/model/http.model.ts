64786587449668dce91dd323de5daf962b1bcc42
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-03-21 11:17:35 +0100

    refactor: http model ts

--- libs/http/src/lib/model/http.model.ts
+++ libs/http/src/lib/model/http.model.ts
@@ -1,34 +1,36 @@
-import { AuthPostModel } from './post/auth-post.model'
 import { ChooseRolePostDtoModel } from './post/controller/choose-role-post-dto.model'
-import { HasRolePostModel } from './post/has-role-post.model'
-import { IsClientPostModel } from './post/is-client-post.model'
-import { IsVetPostModel } from './post/is-vet-post.model'
-import { LogoutPostDtoModel } from './post/controller/logout-post-dto.model'
-import { LoginPostDtoModel } from './post/controller/login-post-dto.model'
-import { RegistrationPostDtoModel } from './post/controller/registration-post-dto.model'
-import { MethodEnum } from '../enum/method.enum'
-import { GetAccountPostModel } from './post/get-account-post.model'
 import {
-  ClinicCreatePostModel,
-  ClinicDeletePostModel,
-  ClinicReadPostModel,
-  ClinicUpdatePostModel,
-} from './post/controller/clinic-post.model'
+  CreateClinicPostDtoModel,
+  DeleteClinicPostDtoModel,
+  ReadClinicPostDtoModel,
+  UpdateClinicPostDtoModel,
+} from './post/controller/clinic-post-dto.model'
+import { LoginPostDtoModel } from './post/controller/login-post-dto.model'
+import { LogoutPostDtoModel } from './post/controller/logout-post-dto.model'
+import { RegistrationPostDtoModel } from './post/controller/registration-post-dto.model'
+import {
+  GetAccountPostDtoModel,
+  HasRolePostDtoModel,
+  IsClientPostDtoModel,
+  IsVetPostDtoModel,
+  ValidTokenPostDtoModel,
+} from './post/guard/guard-post-dto.model'
+import { MethodEnum } from '../enum/method.enum'
 
 type HttpPostType =
-  | AuthPostModel
   | ChooseRolePostDtoModel
-  | HasRolePostModel
-  | IsClientPostModel
-  | IsVetPostModel
+  | CreateClinicPostDtoModel
+  | ReadClinicPostDtoModel
+  | UpdateClinicPostDtoModel
+  | DeleteClinicPostDtoModel
   | LoginPostDtoModel
   | LogoutPostDtoModel
   | RegistrationPostDtoModel
-  | GetAccountPostModel
-  | ClinicCreatePostModel
-  | ClinicReadPostModel
-  | ClinicUpdatePostModel
-  | ClinicDeletePostModel
+  | GetAccountPostDtoModel
+  | HasRolePostDtoModel
+  | IsClientPostDtoModel
+  | IsVetPostDtoModel
+  | ValidTokenPostDtoModel
 
 interface HttpModel {
   method: MethodEnum.post
