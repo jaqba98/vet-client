fc22ffe0f518581fee56b13c288831bb1ae2ed60
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-03-24 15:20:59 +0100

    refactor: table update form

--- libs/form/src/lib/table-form/table-update-form/table-update-form.component.ts
+++ libs/form/src/lib/table-form/table-update-form/table-update-form.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core'
+import { Store } from '@ngrx/store'
 
 import { BaseFormComponent, BaseFormService } from '@vet-client/lib-base-form'
 import { TableCardControlComponent } from '@vet-client/lib-control'
 import { BaseComponentDirective, CrudNotification } from '@vet-client/lib-utils'
 import { DeleteDomainModel } from '@vet-client/lib-domain'
-import { TableFormModel } from '../model/table-form.model'
 import { ClinicTableFormType } from '@vet-client/lib-store'
-import { Store } from '@ngrx/store'
+import { TableFormModel } from '../model/table-form.model'
 
 @Component({
   selector: 'lib-table-update-form',
@@ -18,15 +18,11 @@ export class TableUpdateFormComponent<TFormModel, TDomainModel>
   extends BaseFormService<TableFormModel<TFormModel>, TDomainModel>
   implements OnInit, OnDestroy {
   @Input({ required: true }) formModel!: TableFormModel<TFormModel>
-  @Input({ required: true }) crudNotification!: CrudNotification<TDomainModel, DeleteDomainModel>
+  @Input({ required: true }) crud!: CrudNotification<TDomainModel, DeleteDomainModel>
   @Input({ required: true }) store!: Store<ClinicTableFormType>
 
-  constructor() {
-    super()
-  }
-
   ngOnInit() {
-    this.onInit('Update', this.crudNotification.responseUpdate$)
+    this.onInit('Update', this.crud.responseUpdate$)
     this.initBaseForm(this.formModel)
     this.sub.add(this.store.select('clinicTableForm').subscribe((data) => {
       if (data.selectedRow) {
@@ -40,6 +36,6 @@ export class TableUpdateFormComponent<TFormModel, TDomainModel>
   }
 
   override onSubmit(domain: TDomainModel) {
-    this.crudNotification.runNotificationUpdate(domain)
+    this.crud.runNotificationUpdate(domain)
   }
 }
