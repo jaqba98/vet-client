91de13003f9619b9b3a3d28de2a38c2070cfc767
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-07 22:16:30 +0200

    fix: pet http endpoint.

--- libs/http/src/lib/dom-service/common/pet-http-post.service.ts
+++ libs/http/src/lib/dom-service/common/pet-http-post.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core'
 import { map, take } from 'rxjs'
 import { Store } from '@ngrx/store'
 
-import { DeleteDomainModel, MedicationDomainModel, PetDomainModel } from '@vet-client/lib-domain'
+import { DeleteDomainModel, PetDomainModel } from '@vet-client/lib-domain'
 import { CookieService } from '@vet-client/lib-system'
 import {
-  baseTableFormRowsAction,
+  ActionTypeEnum,
   baseTableFormDeleteAction,
+  baseTableFormMaxPageAction,
+  baseTableFormRowsAction,
   baseTableFormUpdateRow,
   baseTableFormUpdateSelectedRow,
-  baseTableFormMaxPageAction, ActionTypeEnum, PetTableFormType,
+  PetTableFormType,
 } from '@vet-client/lib-store'
 import { HttpExecuteService } from '../../infrastructure/http-execute.service'
 import { ResponseModel } from '../../model/response/response.model'
@@ -17,7 +19,6 @@ import { EndpointEnum } from '../../enum/endpoint.enum'
 import { MethodEnum } from '../../enum/method.enum'
 import { DeleteRequestDtoModel } from '../../model/request/crud/delete-request-dto.model'
 import { TokenRequestDtoModel } from '../../model/base/token-request-dto.model'
-import { MedicationRequestDtoModel } from '../../model/request/controller/medication-request-dto.model'
 import { PetNotification } from '../../notification/pet.notification'
 import { PetRequestDtoModel } from '../../model/request/controller/pet-request-dto.model'
 
@@ -43,7 +44,10 @@ export class PetHttpPostService {
       .pipe(
         take(1),
         map((res) => {
-          this.pet.runResponseCreate({ success: res.success, message: res.messages[0] })
+          this.pet.runResponseCreate({
+            success: res.success,
+            message: res.messages[0],
+          })
         }),
       )
   }
@@ -60,9 +64,15 @@ export class PetHttpPostService {
       .pipe(
         take(1),
         map((res) => {
-          this.store.dispatch(baseTableFormRowsAction<PetDomainModel>(ActionTypeEnum.pet)({
-            rows: res.data.pets.map(row => ({ id: row.id, isSelected: false, row })),
-          }))
+          this.store.dispatch(
+            baseTableFormRowsAction<PetDomainModel>(ActionTypeEnum.pet)({
+              rows: res.data.pets.map(row => ({
+                id: row.id,
+                isSelected: false,
+                row,
+              })),
+            }),
+          )
           this.store.dispatch(baseTableFormMaxPageAction(ActionTypeEnum.pet)())
         }),
       )
@@ -74,20 +84,35 @@ export class PetHttpPostService {
       ...domain,
     }
     return this.httpExecute
-      .exec<ResponseModel<{ pet: PetDomainModel }>>({
+      .exec<ResponseModel<{ pets: PetDomainModel[] }>>({
         method: MethodEnum.post,
         type: { endpoint: EndpointEnum.petUpdate, request },
       })
       .pipe(
         take(1),
         map((res) => {
-          this.store.dispatch(baseTableFormUpdateRow<PetDomainModel>(ActionTypeEnum.pet)({
-            row: { id: res.data.pet.id, isSelected: false, row: res.data.pet },
-          }))
-          this.store.dispatch(baseTableFormUpdateSelectedRow<PetDomainModel>(ActionTypeEnum.pet)({
-            row: { id: res.data.pet.id, isSelected: false, row: res.data.pet },
-          }))
-          this.pet.runResponseUpdate({ success: res.success, message: res.messages[0] })
+          this.store.dispatch(
+            baseTableFormUpdateRow<PetDomainModel>(ActionTypeEnum.pet)({
+              row: {
+                id: res.data.pets[0].id,
+                isSelected: false,
+                row: res.data.pets[0],
+              },
+            }),
+          )
+          this.store.dispatch(
+            baseTableFormUpdateSelectedRow<PetDomainModel>(ActionTypeEnum.pet)({
+              row: {
+                id: res.data.pets[0].id,
+                isSelected: false,
+                row: res.data.pets[0],
+              },
+            }),
+          )
+          this.pet.runResponseUpdate({
+            success: res.success,
+            message: res.messages[0],
+          })
         }),
       )
   }
@@ -105,7 +130,9 @@ export class PetHttpPostService {
       .pipe(
         take(1),
         map(() => {
-          this.store.dispatch(baseTableFormDeleteAction(ActionTypeEnum.pet)({ ids: domain.ids }))
+          this.store.dispatch(
+            baseTableFormDeleteAction(ActionTypeEnum.pet)({ ids: domain.ids }),
+          )
           this.store.dispatch(baseTableFormMaxPageAction(ActionTypeEnum.pet)())
         }),
       )
