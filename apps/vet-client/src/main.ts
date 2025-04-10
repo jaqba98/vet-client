fc966873e6152464129bfc5c4bb34a57024c0a93
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-10 10:51:26 +0200

    refactor: vet-client app project

--- apps/vet-client/src/main.ts
+++ apps/vet-client/src/main.ts
@@ -2,6 +2,4 @@ import { bootstrapApplication } from '@angular/platform-browser'
 
 import { AppComponent, appConfig } from '@vet-client/lib-vet-client'
 
-bootstrapApplication(AppComponent, appConfig).catch(err =>
-  console.error(err),
-)
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err))
