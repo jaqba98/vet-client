79dfef09669d1adf23a9c7c93a8534ebd1a89a6d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-10 13:55:57 +0200

    fix: add http before the apiUrl for testing

--- libs/env/src/lib/environment.ts
+++ libs/env/src/lib/environment.ts
@@ -1,5 +1,5 @@
 import { EnvironmentModel } from './environment.model'
 
 export const environment: EnvironmentModel = {
-  apiUrl: 'localhost:8080/api/v1/',
+  apiUrl: 'http://localhost:8080/api/v1/',
 }
