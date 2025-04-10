722b810c7191c3ca361364f85f388ab0b50b0c3d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-10 13:34:12 +0200

    refactor: env

--- libs/env/src/lib/environment.ts
+++ libs/env/src/lib/environment.ts
@@ -1,5 +1,5 @@
 import { EnvironmentModel } from './environment.model'
 
 export const environment: EnvironmentModel = {
-  apiUrl: 'http://95.49.41.138:8080/api/v1/',
+  apiUrl: 'localhost:8080/api/v1/',
 }
