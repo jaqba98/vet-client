bdd4c75ca45f9fb55775e9f21b614dad72044972
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-14 00:02:26 +0200

    fix: comment out the wrong stories

---
+++ libs/sb/src/lib/page/root/dashboard/dashboard-main/dashboard-main-page.component.stories-old.ts
@@ -0,0 +1,15 @@
+// import type { Meta, StoryObj } from '@storybook/angular'
+//
+// import { DashboardMainPageComponent } from '@vet-client/lib-page'
+// import { StorybookUtilsService } from '../../../../utils/storybook-utils.service'
+//
+// const meta: Meta<DashboardMainPageComponent> = {
+//   component: DashboardMainPageComponent,
+//   title: 'Page/DashboardMain',
+//   ...StorybookUtilsService.getFullscreen(),
+//   ...StorybookUtilsService.getProviders(),
+// }
+// export default meta
+// type Story = StoryObj<DashboardMainPageComponent>
+//
+// export const Default: Story = {}
---
+++ libs/sb/src/lib/page/root/dashboard/vet/vet-services/vet-services-page.component.stories-old.ts
@@ -0,0 +1,15 @@
+// import type { Meta, StoryObj } from '@storybook/angular'
+//
+// import { VetServicesPageComponent } from '@vet-client/lib-page'
+// import { StorybookUtilsService } from '../../../../../utils/storybook-utils.service'
+//
+// const meta: Meta<VetServicesPageComponent> = {
+//   component: VetServicesPageComponent,
+//   title: 'Page/VetServices',
+//   ...StorybookUtilsService.getFullscreen(),
+//   ...StorybookUtilsService.getProviders(),
+// }
+// export default meta
+// type Story = StoryObj<VetServicesPageComponent>
+//
+// export const Default: Story = {}
