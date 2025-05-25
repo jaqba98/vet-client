da3507fa6d35ae0d16af19b16538b85a528d8dde
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-05-25 03:08:39 +0200

    create ability to send text and get json

--- src/app/component/audio-recorder/audio-recorder.component.ts
+++ src/app/component/audio-recorder/audio-recorder.component.ts
@@ -14,7 +14,15 @@ export class AudioRecorderComponent {
 
   isRecording = false;
   audioUrl: string | null = null;
-  transcribedText: any | null = null;
+  transcribedText: any | null = 'Pies rasy Labrador Retriever, 5 lat, samiec, zgłaszany z objawami: apatia, brak apetytu trwający 3 dni, nawracające wymioty, biegunka o luźnej konsystencji, gorączka 39,5°C, nieznaczna odwodnienie. W badaniu klinicznym stwierdzono obniżony odruch ssania, suchą błonę śluzową jamy ustnej oraz nieznaczne powiększenie węzłów chłonnych. Badania laboratoryjne wykazały leukocytozę (WBC 18 000/μl) z przewagą neutrofili, co sugeruje aktywny proces zapalny. W badaniu kału potwierdzono obecność pasożytów jelitowych – Toxocara canis. Ultrasonografia jamy brzusznej wykazała nieznaczne powiększenie wątroby i śledziony, bez widocznych ognisk martwicy czy zmian ogniskowych.\n' +
+    '\n' +
+    'Rozpoznanie: infekcyjne zapalenie jelit wtórne do inwazji pasożytów jelitowych (Toxocara canis), prowadzące do ostrego stanu zapalnego przewodu pokarmowego.\n' +
+    '\n' +
+    'Zalecenia terapeutyczne: podanie fenbendazolu w dawce 50 mg/kg przez 3 dni jako lek przeciwpasożytniczy; wprowadzenie diety lekkostrawnej, bogatej w łatwo przyswajalne białka i węglowodany, podawanej w małych porcjach, aby zmniejszyć podrażnienie przewodu pokarmowego; intensywne nawadnianie doustne lub dożylne w przypadku znacznego odwodnienia; kontrola kliniczna i laboratoryjna po 7 dniach celem oceny skuteczności leczenia oraz ewentualnej modyfikacji terapii; w przypadku utrzymania się lub nasilenia objawów (np. przedłużających się wymiotów, krwistej biegunki, wzrostu temperatury ciała) wskazana jest ponowna konsultacja i dalsza diagnostyka, w tym możliwość wykonania posiewu bakteriologicznego lub biopsji jelit.\n' +
+    '\n' +
+    'Dodatkowo zaleca się profilaktykę przeciwpasożytniczą co najmniej 2 razy w roku oraz regularne badania kału, aby zapobiec nawrotom inwazji. W przypadku obecności innych zwierząt w gospodarstwie warto przeprowadzić badania przesiewowe i ewentualne leczenie także ich.';
+
+  analysisResult: any;
 
   constructor(private ngZone: NgZone, private http: HttpClient) {}
 
@@ -72,12 +80,44 @@ export class AudioRecorderComponent {
     this.http.post('http://localhost:8080/api/speech-to-text', formData, { responseType: 'text' })
       .subscribe({
         next: response => {
-          this.transcribedText = JSON.parse(response).DisplayText;
+          const result = JSON.parse(response);
+          this.transcribedText = result.DisplayText;
+
+          // od razu analizujemy tekst przez drugi request
+          const analyzeUrl = 'http://localhost:8080/api/analyze';
+          const analyzePayload = { text: this.transcribedText };
+
+          this.http.post<any>(analyzeUrl, analyzePayload).subscribe({
+            next: analysis => {
+              console.log('Wynik analizy tekstu:', analysis);
+              this.analysisResult = analysis; // Możesz użyć tej zmiennej w szablonie
+            },
+            error: err => {
+              console.error('Błąd analizy tekstu:', err);
+            }
+          });
         },
         error: err => {
           this.transcribedText = 'Błąd podczas rozpoznawania mowy.';
           console.error(err);
         }
       });
+  }
+
+
+  aaa() {
+    // od razu analizujemy tekst przez drugi request
+    const analyzeUrl = 'http://localhost:8080/api/analyze';
+    const analyzePayload = { text: this.transcribedText };
+
+    this.http.post<any>(analyzeUrl, analyzePayload).subscribe({
+      next: analysis => {
+        console.log('Wynik analizy tekstu:', analysis);
+        this.analysisResult = analysis; // Możesz użyć tej zmiennej w szablonie
+      },
+      error: err => {
+        console.error('Błąd analizy tekstu:', err);
+      }
+    });
   }
 }
