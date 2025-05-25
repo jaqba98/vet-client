968f3c256c79fe79f8a93400f72eb6d63ce65033
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-05-25 04:36:13 +0200

    add basic login view styles

--- src/app/view/login/login-view.component.ts
+++ src/app/view/login/login-view.component.ts
@@ -3,10 +3,13 @@ import { Router } from '@angular/router';
 
 import { MsalService } from '../../msal/msal.service';
 import { RouteEnum } from '../../enum/route.enum';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'login-view',
+  imports: [CommonModule],
   templateUrl: './login-view.component.html',
+  styleUrls: ['./login-view.component.scss']
 })
 export class LoginViewComponent {
   constructor(
@@ -14,8 +17,17 @@ export class LoginViewComponent {
     private router: Router,
   ) {}
 
-  async login() {
-    await this.msalService.login();
-    await this.router.navigate([RouteEnum.profile]);
+  login() {
+    this.msalService.login()
+      .then(() => {
+        this.router.navigate([RouteEnum.profile]);
+      })
+      .catch((error) => {
+        if (error.errorCode === 'user_cancelled' || error.message?.includes('user_cancelled')) {
+          console.log('Login cancelled by user.');
+        } else {
+          console.error('Login error:', error);
+        }
+      });
   }
 }
